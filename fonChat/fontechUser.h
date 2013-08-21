2da0ec4680af76aa9287e45c3a401073e0f495b7
icelorc <icelorc@gmail.com>
2013-08-21 14:56:29 +0800

    add fontechUser to the project

---
+++ fonChat/fontechUser.h
@@ -0,0 +1,13 @@
+//
+//  fontechUser.h
+//  fonChat
+//
+//  Created by Robert Huang on 8/21/13.
+//  Copyright (c) 2013 Fontech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fontechUser : NSObject
+
+@end
---
+++ fonChat/fontechUser.m
@@ -0,0 +1,13 @@
+//
+//  fontechUser.m
+//  fonChat
+//
+//  Created by Robert Huang on 8/21/13.
+//  Copyright (c) 2013 Fontech. All rights reserved.
+//
+
+#import "fontechUser.h"
+
+@implementation fontechUser
+
+@end
